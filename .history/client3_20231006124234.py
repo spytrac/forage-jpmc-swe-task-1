d9dbac4cbeac0fb02a2d956c8bbb7828f993155c
spytrac <114122236+spytrac@users.noreply.github.com>
2023-10-06 12:54:15 -0700

    Apply updates to meet requirements

--- .history/client3_20231006124234.py
+++ .history/client3_20231006124234.py
@@ -42,8 +42,6 @@ def getDataPoint(quote):
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
     """ ------------- Update this function ------------- """
-    if (price_b==0):
-        return
     return price_a/price_b
 
 
@@ -54,10 +52,8 @@ if __name__ == "__main__":
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
 
         """ ----------- Update to get the ratio --------------- """
-        prices = {}
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
-            prices[stock]= price
             print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
 
-        print("Ratio %s" % getRatio(prices["ABC"], prices["DEF"]))
+        print("Ratio %s" % getRatio(price, price))
